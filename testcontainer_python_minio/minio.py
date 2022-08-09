bd4fc3eb995481d16e0e49f8e36d945bd98f1104
Malte Hedderich <malte.hedderich@serviceware.de>
2022-08-09 17:52:33 +0200

    remove custom error

--- testcontainer_python_minio/minio.py
+++ testcontainer_python_minio/minio.py
@@ -1,5 +1,7 @@
 import logging
 import os
+from urllib.error import HTTPError
+from warnings import catch_warnings
 
 from requests import Response, exceptions, get, post
 from testcontainers.core.container import DockerContainer
@@ -9,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class MinioContainer(DockerContainer):
-    def __init__(self, image="minio/minio:RELEASE.2020-03-09T18-26-53Z"):
+    def __init__(self, image="minio/minio:RELEASE.2022-08-08T18-34-09Z"):
         super(MinioContainer, self).__init__(image)
 
         self.with_exposed_ports(9000).with_env("MINIO_ACCESS_KEY", "testtest").with_env(
@@ -22,17 +24,12 @@ class MinioContainer(DockerContainer):
     def secretKey(self):
         return self.env["MINIO_SECRET_KEY"]
 
-    @wait_container_is_ready(exceptions.ConnectionError)
+    @wait_container_is_ready(exceptions.ConnectionError, exceptions.ReadTimeout)
     def _connect(self):
-        try:
-            logger.info("Connecting to %s", self.get_url())
-            res: Response = get(self.get_url())
-            if res.status_code >= 500:
-                logger.info("Wait call received %s status code", res.status_code)
-                raise Exception("Not ready")
-        except Exception as e:
-            logger.debug("Failed to connect", exc_info=True)
-            raise e
+        url = f'{self.get_url()}/minio/health/live'
+        logger.info("Connecting to %s", url)
+        res: Response = get(url)
+        res.raise_for_status()
 
     def get_url(self):
         port = self.get_exposed_port(9000)
